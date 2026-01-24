666a60708fd38a402e31dc0adbe772079eff5993
sarthak29-ai <sarthakshende.29@gmail.com>
2026-01-24 18:32:02 +0530

    3rd fm project deploy

---
+++ faq-accordion-main/tailwind.config.js
@@ -0,0 +1,13 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["*.html"],
+  theme: {
+    extend: {
+   fontFamily: {
+        'work': ['Work Sans', 'sans-serif'],
+      },
+   },
+  },
+  plugins: [],
+}
+
